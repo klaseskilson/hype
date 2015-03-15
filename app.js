93943332e1dae486f5e51a14d9176edbf6f68786
Klas Eskilson <klas.eskilson@gmail.com>
2015-03-15 22:46:41 +0100

    Slim down router structure

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// load routes
+var routes = require('./routes');
 
 var app = express();
 
@@ -23,8 +23,8 @@ app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// setup routes
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
