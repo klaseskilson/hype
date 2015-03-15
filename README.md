9c812c5bc774abd07ed1fcd20f615561794c077a
Klas Eskilson <klas.eskilson@gmail.com>
2015-03-15 22:59:25 +0100

    Use the route separation structure

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 Simple tool for creating a hype around an event or similar.
 
+It basically follows the [Route separation example](https://github.com/strongloop/express/tree/dee9fbbbda2f2483c657cf912f68db1a5ba1fcac/examples/route-separation).
 
 ## Setup
 
--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+/*
+ * modules
+ */
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,9 +8,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-// load routes
-var routes = require('./routes');
+// load our files
+var site = require('./site');
 
+// wooop
 var app = express();
 
 // view engine setup
@@ -24,7 +28,7 @@ app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // setup routes
-app.use('/', routes);
+app.get('/', site.index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
