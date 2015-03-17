7a3de6778cf6217204b22475568a11b25d9b8c54
Klas Eskilson <klas.eskilson@gmail.com>
2015-03-17 11:54:58 +0100

    Add angular to frontend

---
+++ public/javascripts/main.js
@@ -0,0 +1,30 @@
+var hypeApp = angular.module('hypeApp', []);
+
+hypeApp.controller('ImageCtrl', ['$scope', '$http', '$window', function($scope, $http, $window) {
+  $scope.loading = true;
+  $scope.img = '';
+
+  $http.post('/image/create')
+    .success(function(data) {
+      console.log(data);
+      $scope.img = data.image;
+      $scope.loading = false;
+    })
+    .error(function(data) {
+      console.log('error!', data);
+    });
+
+  $scope.saveImage = function() {
+    console.log('saving!');
+    $http.post('/image/save')
+      .success(function(data) {
+        console.log(data);
+
+        // $window.location.href = 'https://facebook.com/photo.php?fbid='+data.image.id+'&makeprofile=1';
+      })
+      .error(function(data) {
+        console.log('an error occured', data);
+      });
+  };
+
+}]);
